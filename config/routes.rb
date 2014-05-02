c1fd713053c59cd8999b6606ecea13bfe9f87651
Kamen Hursev <kamen.hursev@gmail.com>
2014-05-02 18:17:45 +0300

    Add an empty Companies controller
    remove a coffee file
    
    rename

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'companies/new'
+
   get 'register' => 'brokers#select', as: :start_registration
   post 'brokers/submit', as: :submit_brokers
 
-  resources :company
+  resources :companies
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
