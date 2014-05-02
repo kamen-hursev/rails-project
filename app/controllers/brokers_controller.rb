57e8406960936a6884e82f85b171aa013155232b
Kamen Hursev <kamen.hursev@gmail.com>
2014-05-02 18:13:11 +0300

    [Refactoring] use better routes and controllers

--- app/controllers/brokers_controller.rb
+++ app/controllers/brokers_controller.rb
@@ -1,14 +1,14 @@
-class RegisterController < ApplicationController
-  def select_brokers
+class BrokersController < ApplicationController
+  def select
     @brokers = Broker.all
   end
 
-  def submit_brokers
-    company = Company.new
-    company.broker_ids = params[:brokers]
+  def submit
+    company = Company.new(broker_ids: params[:brokers])
     company.validate_number_of_brokers
     respond_to do |format|
       if company.errors[:brokers].empty?
+        session[:brokers] = params[:brokers]
         format.json { render json: { success: true } }
       else
         format.json { render json: { success: false, error: company.errors[:brokers][0] } }
